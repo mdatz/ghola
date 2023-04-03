83c69794a9646233adbeb484d254c4c8f9536ec4
mdatz <kadatzmatthew@gmail.com>
2023-04-02 18:11:13 -0600

    Fixed bad useEffect causing weird routing behaviours

--- pages/chat.tsx
+++ pages/chat.tsx
@@ -3,12 +3,10 @@ import { Header } from '../components/General/Header/Header';
 import { ConversationPanels } from '../components/Panels/ConversationPanels/ConversationPanels';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
-import { useConversationContext } from '../context/ConversationContext';
 
 
 export default function Chat() {
 
-    const { selectedProfile } = useConversationContext();
     const { data: session, status } = useSession();
     const router = useRouter();
 
@@ -17,12 +15,6 @@ export default function Chat() {
             router.push('/login');
         }
     }, [status]);
-
-    useEffect(() => {
-        if (selectedProfile === null) {
-            router.push('/dashboard');
-        }
-    });
 
     return (
         <div className='fullscreen'>
